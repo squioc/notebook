1e1c24639f97c066879ead041fa55ef6097ecff9
Quioc Sébastien <sebastien.quioc@yahoo.com>
2017-08-15 20:40:12 +0200

    feat(notebook): adds the Note structure

---
+++ note.go
@@ -0,0 +1,19 @@
+package notebook
+
+// Note is a struct which represents
+// a information to keep
+type Note struct {
+    // The category of the note
+    Category string
+
+    // The content of the note
+    Content []byte
+}
+
+// NewNote creates a new note from a category and a content
+func NewNote(Category string, Content []byte) Note {
+    return &note {
+        Category: category,
+        Content: content,
+    }
+}
