4ffd86c241a1a24946b2a3a8f454d3c62e42d7e9
Quioc Sébastien <sebastien.quioc@yahoo.com>
2017-08-17 07:51:31 +0200

    fix(notebook): fixes a comment in the Notebook interface

--- notebook.go
+++ notebook.go
@@ -3,7 +3,7 @@ package notebook
 // Notebook is an interface which represents
 // a collection of notes
 type Notebook interface {
-    // List returns the list of notes holding in the notebook
+    // List returns a list of notes holding in the notebook
     List() Notes
 
     // Get returns a note from the notebook according to its name
